9f6dc7d4a1ae92c578be9f26f0d88fa673c9c91e
NSNirvana <NSNirvana@iCloud.com>
2015-07-11 20:37:22 +0800

    Initial Commit

---
+++ NirBillboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NirBillboard
+//
+//  Created by Nirvana on 7/11/15.
+//  Copyright © 2015 NSNirvana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
