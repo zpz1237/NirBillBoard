0038957b632d6d6fba288e9f0dd688289c76de3d
NSNirvana <NSNirvana@iCloud.com>
2015-07-13 16:47:55 +0800

    ExploreView中ScrollView界面切换已完成，即将添加示例数据

--- NirBillboard/ExploreViewController.swift
+++ NirBillboard/ExploreViewController.swift
@@ -9,11 +9,48 @@
 import UIKit
 
 class ExploreViewController: UIViewController {
-
+    var pageMenu: CAPSPageMenu?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.navigationController?.navigationBar.translucent = false
+        
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        
+        var controllerArray: [UIViewController] = []
+        
+        let ExploreAViewController = storyBoard.instantiateViewControllerWithIdentifier("ExploreAViewID")
+        ExploreAViewController.title = "户外探险"
+        controllerArray.append(ExploreAViewController)
+        let ExploreBViewController = storyBoard.instantiateViewControllerWithIdentifier("ExploreBViewID")
+        ExploreBViewController.title = "二次元"
+        controllerArray.append(ExploreBViewController)
+        let ExploreCViewController = storyBoard.instantiateViewControllerWithIdentifier("ExploreCViewID")
+        ExploreCViewController.title = "数码控"
+        controllerArray.append(ExploreCViewController)
+        
+        let parameters: [CAPSPageMenuOption] = [
+            .MenuItemSeparatorWidth(4.3),
+            .ScrollMenuBackgroundColor(UIColor.whiteColor()),
+            .ViewBackgroundColor(UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)),
+            .BottomMenuHairlineColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 0.1)),
+            .SelectionIndicatorColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
+            .MenuMargin(20.0),
+            .MenuHeight(40.0),
+            .SelectedMenuItemLabelColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
+            .UnselectedMenuItemLabelColor(UIColor(red: 40.0/255.0, green: 40.0/255.0, blue: 40.0/255.0, alpha: 1.0)),
+            .MenuItemFont(UIFont(name: "HelveticaNeue-Medium", size: 14.0)!),
+            .UseMenuLikeSegmentedControl(true),
+            .MenuItemSeparatorRoundEdges(true),
+            .SelectionIndicatorHeight(2.0),
+            .MenuItemSeparatorPercentageHeight(0.1)
+        ]
+        
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 0.0, self.view.frame.width, self.view.frame.height), pageMenuOptions: parameters)
+        
+        self.view.addSubview(pageMenu!.view)
+        
     }
 
     override func didReceiveMemoryWarning() {
